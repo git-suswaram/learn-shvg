5573dc523b1e140be33237f3e989b84758211fe0
suswaram <git.suswaram@gmail.com>
2018-10-31 03:49:54 -0500

    Committing missing file

---
+++ src/main/java/com/shvg/frameworks/springboot/restapiwithjpa/repository/JPostRepository.java
@@ -0,0 +1,7 @@
+package com.shvg.frameworks.springboot.restapiwithjpa.repository;
+
+import com.shvg.frameworks.springboot.restapiwithjpa.entity.JPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JPostRepository extends JpaRepository<JPost,Integer> {
+}
